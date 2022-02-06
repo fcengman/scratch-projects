1a0b49b57ec40543d3f2b12365fe0fef25244d94
Freya Engman <fcengman@gmail.com>
2022-02-06 10:10:03 -0800

    Cleaned directory & added gitignore

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-while True:
-        new_game.board.opponent_turn()
-        if new_game.check_end_game():
-            break
-        else:
-            continue
-    print(new_game)
-    print('Ok, Bye')
